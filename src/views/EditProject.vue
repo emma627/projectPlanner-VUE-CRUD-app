7428e1f0a1ed21d2f0b365f4b4ff7f7c95bb6051
jinyan YANG <jinyan.yang627@gmail.com>
2021-03-31 14:03:13 +0200

    update done

--- src/views/EditProject.vue
+++ src/views/EditProject.vue
@@ -10,6 +10,34 @@
 
 <script>
 export default {
+  props:["id"],
+  data(){
+    return{
+      title:"",
+      details:""
+    }
+  },
+  mounted(){
+    fetch('http://localhost:5000/projects/'+this.id)
+    .then(res=>res.json())
+    .then(data=>{
+      this.title=data.title
+      this.details=data.title
+    })
+    .catch(err=>console.log(err))
+  },
+  methods:{
+  handleSubmit(){
+    fetch('http://localhost:5000/projects/'+this.id, {
+      method: "PATCH",
+      headers:{"Content-type":"application/json"},
+      body:JSON.stringify({title:this.title, details:this.details})
+    })
+    .then(this.$router.push("/"))
+    .catch(err=>console.log(err))
+    
+  }
+  }
 
 }
 </script>
